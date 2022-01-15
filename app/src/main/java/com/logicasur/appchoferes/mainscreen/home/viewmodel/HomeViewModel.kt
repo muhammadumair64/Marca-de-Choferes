717d367a5c0dc35a9ebf068a0ac9e664ab2dcb5a
muhammadumair64 <84626460+muhammadumair64@users.noreply.github.com>
2022-01-15 20:23:43 +0500

    with error

--- app/src/main/java/com/logicasur/appchoferes/mainscreen/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/logicasur/appchoferes/mainscreen/home/viewmodel/HomeViewModel.kt
@@ -26,6 +26,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.viewModelScope
 import com.logicasur.appchoferes.Extra.CheckConnection
 import com.logicasur.appchoferes.Extra.K
